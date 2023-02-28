29665a3882e1e5ee145c919767daba40a2466a91
ramenoff <39674652+ramenoff@users.noreply.github.com>
2023-02-28 01:58:46 -0500

    Main functionality of calculator done. Needs limited decimals, backspace, clear, and aesthetic upgrades.

--- calculator.js
+++ calculator.js
@@ -3,6 +3,10 @@
 numberInput = [];
 numList = [];
 mathsign = "";
+ans;
+decimalCount = false;
+
+
 
 function enterOne(){
 
@@ -22,14 +26,14 @@ function enterThree(){
 
     numberInput.push("3");
     document.getElementById("screen").innerHTML = numberInput.join("");
-    
+
 }
 
 function enterFour(){
 
     numberInput.push("4");
     document.getElementById("screen").innerHTML = numberInput.join("");
-    
+
 }
 
 function enterFive(){
@@ -43,7 +47,7 @@ function enterSix(){
 
     numberInput.push("6");
     document.getElementById("screen").innerHTML = numberInput.join("");
-    
+
 }
 
 function enterSeven(){
@@ -51,19 +55,21 @@ function enterSeven(){
     numberInput.push("7");
     document.getElementById("screen").innerHTML = numberInput.join("");
 
+
 }
 
 function enterEight(){
 
     numberInput.push("8");
     document.getElementById("screen").innerHTML = numberInput.join("");
-    
+
 }
 
 function enterNine(){
 
     numberInput.push("9");
     document.getElementById("screen").innerHTML = numberInput.join("");
+
     
 }
 
@@ -74,34 +80,114 @@ function enterZero(){
     
 }
 
+function enterDecimal(){
+
+    if(decimalCount === false){
+
+        numberInput.push(".");
+        document.getElementById("screen").innerHTML = numberInput.join("");
+        decimalCount = true;
+
+    }
+    
+
+}
+   
+
 function enterPlus(){
 
+    decimalCount = false;
     numList.push(numberInput.join(""));
+    calculate();
     mathsign = "+";
     numList.push(mathsign);
     numberInput = [];
-    alert(numList);
+
+}
+
+function enterMinus(){
+
+    decimalCount = false;
+    numList.push(numberInput.join(""));
+    calculate();
+    mathsign = "-";
+    numList.push(mathsign);
+    numberInput = [];
+
+}
+
+function enterTimes(){
+
+    decimalCount = false;
+    numList.push(numberInput.join(""));
+    calculate();
+    mathsign = "*";
+    numList.push(mathsign);
+    numberInput = [];
+
+}
+
+
+function enterDivide(){
+
+    decimalCount = false;
+    numList.push(numberInput.join(""));
+    calculate();
+    mathsign = "/";
+    numList.push(mathsign);
+    numberInput = [];
 
 }
 
 function enterEquals(){
 
+    decimalCount = false;
     numList.push(numberInput.join(""));
     checkMathsign();
+    
+}
 
+function calculate(){
+
+    if(numList.length === 3){
+
+        checkMathsign();
+        numList = [];
+        numList[0] = ans;
+
+    }
+    
 }
 
 function checkMathsign(){
 
     if(mathsign == "+"){
 
-        document.getElementById("screen").innerHTML = parseFloat(numList[0]) + parseFloat(numList[2]);
+        ans = parseFloat(numList[0]) + parseFloat(numList[2]);
 
     }
 
+    if(mathsign == "-"){
+
+       ans = parseFloat(numList[0]) - parseFloat(numList[2]);
+
+    }
+
+    if(mathsign == "*"){
+
+        ans = parseFloat(numList[0]) * parseFloat(numList[2]);
+
+    }
+
+    if(mathsign == "/"){
+
+        ans = parseFloat(numList[0]) / parseFloat(numList[2]);
+
+    }
+
+    document.getElementById("screen").innerHTML = ans;
 
 }
-
 
 
 
